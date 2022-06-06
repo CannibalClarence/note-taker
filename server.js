95efc26d6c69b7452224bd1f6ddd7e09b5cbc09d
Theo Phillips <phillipstheo88@gmail.com>
2022-06-05 20:17:13 -0700

    reworked server and js and css

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// importing dependencies to interact with the front end
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -6,7 +7,7 @@ const fs = require("fs");
 const app = express();
 
 // Setting a port listener
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 //  createNoteData Array
 let createNoteData = [];
